c1d06c8cae471ce8b1f9d15dcc7ab641a0bb874c
junisanjaya2026 <EMAIL_GITHUB_KAMU>
2025-12-04 00:19:10 +0700

    Add ItemFactory and update UserFactory with additional fields

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,9 +26,11 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'username' => fake()->unique()->userName(),
+            'fullname' => fake()->name(),
+            'phone' => fake()->phoneNumber(),
+            'role_id' => fake()->numberBetween(1, 3),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
         ];
     }
 
