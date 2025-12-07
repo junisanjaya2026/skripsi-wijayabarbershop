56c54a877b07e7e7c46ee9b3e809dca05a5679ce
junisanjaya2026 <EMAIL_GITHUB_KAMU>
2025-12-08 01:34:14 +0700

    Add Bootstrap SCSS mixins and layout files for customer interface
    - Introduced various SCSS mixins for grid, images, lists, pagination, text reset, resizing, table variants, text truncation, transitions, utilities, and visually hidden elements.
    - Created layout files for header, footer, and navbar to structure the customer-facing pages.
    - Implemented a master layout to include header, footer, and scripts for the customer interface.
    - Updated web routes to include menu and cart functionalities, linking them to the MenuController.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MenuController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('customer.home');
 });
+
+// Route::get('/', [MenuController::class, 'index'])->name('menu');
+Route::get('/menu', [MenuController::class, 'index'])->name('menu');
+Route::get('/cart', [MenuController::class, 'cart'])->name('cart');
+Route::post('/add-to-cart', [MenuController::class, 'addToCart'])->name('add.to.cart');
+Route::post('/update-cart', [MenuController::class, 'updateCart'])->name('update.cart');
+Route::post('/remove-from-cart', [MenuController::class, 'removeFromCart'])->name('remove.from.cart');
+Route::get('/clear-cart', [MenuController::class, 'clearCart'])->name('clear.cart');
+
+Route::get('/checkout', function () {
+    return view('customer.checkout');
+})->name('checkout');
